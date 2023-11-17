6a2402164c59c83004813e26d5f7127229f9d96c
hirakero <k16.h.white@gmail.com>
2023-11-17 14:33:43 +0900

    勝者の宣言

--- src/tic_tac_toe_helix/core.cljs
+++ src/tic_tac_toe_helix/core.cljs
@@ -10,15 +10,39 @@
              :on-click on-click} 
             value))
 
+(defn calcurate-winner [squares]
+  (let [lines [[0 1 2]
+               [3 4 5]
+               [6 7 8]
+               [0 3 6]
+               [1 4 7]
+               [2 5 8]
+               [0 4 8]
+               [2 4 6]]]
+    (some-> (filter (fn [[a b c]]
+                      (and (squares a)
+                           (= (squares a)
+                              (squares b)
+                              (squares c))))
+                    lines)
+            first
+            (#(squares (first %))))))
+
 (defnc board [] 
   (let [[x-is-next? set-x-is-next?] (hooks/use-state true)
         [squares set-squares] (hooks/use-state (vec (repeat 9 nil))) 
+        winner (calcurate-winner squares)
+        status (if winner
+                 (str "Winner: " winner)
+                 (str "Next player: " (if x-is-next? "X" "O")))
         handle-click (fn [i]
-                       (when (nil? (get squares i))
+                       (when-not (or (get squares i)
+                                      winner)  
                          (set-squares (assoc squares i (if x-is-next? "X" "O")))
                          (set-x-is-next? (not x-is-next?))))] 
     
     (<>
+     (d/div {:class-name "status"} status)
      (d/div {:class-name "board-row"}
             ($ square {:value (get squares 0) :on-click #(handle-click 0)})
             ($ square {:value (get squares 1) :on-click #(handle-click 1)})
